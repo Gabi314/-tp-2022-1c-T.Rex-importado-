360ece4ee9b96a043b8f044155f9269786e0ac4f
Leonardo Corrales <leonardo.guido1999@gmail.com>
2022-05-09 12:55:36 -0300

    Creo el modulo consola

---
+++ consola/src/consola.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : consola.c
+ Author      : Leo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Cree el modulo consola!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
